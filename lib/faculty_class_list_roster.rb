180d8f12f9aae7b56a890d40b39b67e159b219ab
Joe <jmpease21@gmail.com>
2013-09-18 08:41:30 -0400

    Update faculty_class_list_roster.rb

--- lib/faculty_class_list_roster.rb
+++ lib/faculty_class_list_roster.rb
@@ -5,7 +5,7 @@ m = Mysql.new("localhost","root","passwd")
 r = m.query("SELECT person.last_name, person.first_name, person_email.email, person_phone.phone_number, addresses.address
 	    FROM registration
 	    	left join person on person.person_serial = registration.person_serial
-		left join class on regs.class_serial = class.class_serial
+		left join class on registration.class_serial = class.class_serial
 		left join person_email on email.person_serial = person.person_serial
 		left join addresses on addresses.link_serial = person.person_serial
 		left join person_phone on person_phone.person_serial = person.person_serial
